3dcf3dab5a73658b22019087bb7bc63ab25bbba3
arsalan-anwari <arsalan@anwari.nl>
2026-08-17 15:40:54 +0200

    Change page title devider char to be "|" by default.

--- src/sphinx_2009scape_theme/__init__.py
+++ src/sphinx_2009scape_theme/__init__.py
@@ -16,7 +16,7 @@ if TYPE_CHECKING:
     from sphinx.application import Sphinx
     from sphinx.util.typing import ExtensionMetadata
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __all__ = ["__version__", "setup"]
 
 THEME_PATH = (Path(__file__).parent / "theme" / "2009scape").resolve()
